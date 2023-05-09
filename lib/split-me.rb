54e2be88913748a1872a8687fafee839aee6e04c
Joel Howard <joelhoward2@gmail.com>
2023-05-09 16:37:47 -0400

    Modify lib/split-me.rb

--- lib/split-me.rb
+++ lib/split-me.rb
@@ -1,6 +1,6 @@
 class Board
-  attr_accessor :board
-  # 2d array with board state
+  attr_reader :board, :red, :black
+  
   def initialize
     @dark_bg = "\e[48;5;253m"
     @light_bg = "\e[48;5;231m"
@@ -10,26 +10,21 @@ class Board
     @blank = '   '
 
     @red = {
-      K: ' ♚ ',
-      Q: ' ♛ ',
-      R: ' ♜ ',
-      B: ' ♝ ',
-      N: ' ♞ ',
-      P: ' ♟︎ ',
-    }.transform_values{ |value| @red_fg + value + @reset }
+      K: '♚',
+      Q: '♛',
+      R: '♜',
+      B: '♝',
+      N: '♞',
+      P: '♟︎',
+    }.transform_values{ |value| @red_fg + value }
 
     @black = @red.transform_values{ |value| @black_fg + value[5..-1] }
 
-    # @row = array = Array.new(8) { |i| i.even? ? @light_bg + @blank + @reset : @dark_bg + @blank + @reset }
-    
-    # @board = [@row, @row.reverse] * 4
-
-    @home = [' ♜ ', ' ♞ ', ' ♝ ', ' ♛ ', ' ♚ ', ' ♝ ', ' ♞ ', ' ♜ ']
     @pawns = [' ♟︎ ' * 8]
 
   end
 
-  def empty_board
+  def build_empty_board
     @board = Array.new(8) do |row_i|
       Array.new(8) do |el_i|
         (row_i.even? == el_i.even? ? @light_bg : @dark_bg) + @blank + @reset
@@ -37,6 +32,20 @@ class Board
     end
   end
   
+  def build_back_rank(color, rank)
+    home = [:R, :N, :P, :Q, :K, :B, :N, :R]
+    i = 0
+    @board[rank].each do |square|
+      square[-6] = color[home[i]]
+      i += 1
+    end
+  end
+
+  def build_second_rank(color, rank)
+    @board[rank].each do |square|
+      square[-6] = color[:P]
+    end
+  end
 
   def print_board
     @board.each do |row|
@@ -60,34 +69,15 @@ class Game
   # contains looping script to play game
 end
 
-# dark_bg = "\e[48;5;253m"
-# light_bg = "\e[48;5;231m"
-# black_fg = "\e[30m"
-# red_fg = "\e[31m"
-
-
-# reset = "\e[0m"
-
-
-
-# def print_row(d, l, r, piece)
-#   4.times { print l + d + piece + r}
-#   print r
-#   puts ''
-# end
-
-# def print_middle(d, l, r, piece)
-#   4.times do
-#     print_row(d, l, r, piece)
-#     print_row(l, d, r, piece)
-#   end
-# end
 
 # print_middle(dark_bg, light_bg, reset, pieces[:blank])
 # puts 'a b c d e f g h'
 
 board = Board.new
-board.empty_board
-board.board[0][0][-6] = 'z'
+board.build_empty_board
+board.board[0][0][-6] = board.black[:K]
+board.build_back_rank(board.black, 0)
+board.build_back_rank(board.red, 7)
+board.build_second_rank(board.black, 1)
+board.build_second_rank(board.red, 6)
 board.print_board
-pp board.board[0][0][-6]
