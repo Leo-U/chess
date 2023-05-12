49490fe40c69e8debd97e9eafde0064bf1a6172f
Joel Howard <joelhoward2@gmail.com>
2023-05-11 23:32:41 -0400

    Add lib/oop_version.rb

---
+++ lib/oop_version.rb
@@ -0,0 +1,34 @@
+# Make Board, but initialize Piece instances in the 2d array.
+
+class Board
+  attr_reader :state
+  def initialize
+    @state =  8.times.map { 8.times.map { nil } }
+  end
+
+  def add_piece(piece, x, y)
+    @state[x][y] = piece
+  end
+
+  def add_first_rank(rank, color)
+    pieces = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+    @state[rank].each_index do |i|
+      add_piece(Piece.new(pieces[i], 'black'), rank, i)
+    end
+  end
+
+end
+
+
+class Piece
+  def initialize(piece_name, color)
+    @piece_name = piece_name
+    @color = color
+  end
+end
+
+
+
+board = Board.new
+board.add_first_rank(0, 'black')
+pp board.state
