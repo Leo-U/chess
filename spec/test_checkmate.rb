fb60111142faa557938bf4c0eb33ee2944342529
Joel Howard <joelhoward2@gmail.com>
2023-06-08 19:30:18 -0400

    Modify spec/test_checkmate.rb

--- spec/test_checkmate.rb
+++ spec/test_checkmate.rb
@@ -1,24 +1,19 @@
 require_relative '../lib/oop_version.rb'
 
 describe Game do
-  # Read each game from the PGN file
-  File.open('./game-bank-mate-only.pgn').each("}\n\n") do |game_text|
+  File.open('./spec/game-bank-all-end-in-mate.pgn').each("}\n\n") do |game_text|
 
     it 'plays the game correctly' do
-      # Extract each move and FEN from the game
-      game_moves = game_text.split(/\s/)
-                            .drop_while { |s| !s.match(/[a-h][1-8][a-h][1-8]|o-o(-o)?/) }
-                            .take_while { |s| !s.match(/\{/) }
+      game_moves = game_text
+      .split(/\s/)
+      .drop_while { |s| !s.match(/[a-h][1-8][a-h][1-8]|o-o(-o)?/) }
+      .take_while { |s| !s.match(/\{/) }
       fen_string = game_text.split(/\s{ \s*/).last.split[0]
-      
-      p game_moves
-      puts fen_string
-      # Play the game
+
       game = Game.new
       allow_any_instance_of(Input).to receive(:get_input).and_return(*game_moves)
       game.play_game
 
-      # Verify the final FEN matches
       expect(game.board.output_fen).to eq(fen_string)
     end
   end
