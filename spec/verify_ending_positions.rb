2c869335541b7ff148f5ed8b1253c8361624d578
Joel Howard <joelhoward2@gmail.com>
2023-06-14 19:57:44 -0400

    Add spec/verify_ending_positions.rb

---
+++ spec/verify_ending_positions.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/oop_version.rb'
+
+describe Game do
+  File.open('./spec/game-bank-all-end-in-mate.pgn').each("}\n\n") do |game_text|
+
+    it 'plays the game correctly' do
+      game_moves = game_text
+      .split(/\s/)
+      .drop_while { |s| !s.match(/[a-h][1-8][a-h][1-8]|o-o(-o)?/) }
+      .take_while { |s| !s.match(/\{/) }
+      fen_string = game_text.split(/\s{ \s*/).last.split[0]
+
+      game = Game.new
+      allow_any_instance_of(Input).to receive(:get_input).and_return(*game_moves)
+      game.play_game
+
+      expect(game.board.output_fen).to eq(fen_string)
+    end
+  end
+end
